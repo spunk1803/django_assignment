e5f126819ce94b081db3aff9697b5cdba16ace04
ankush <ankush.1996@gmail.com>
2015-06-28 00:32:53 +0530

    views made

--- contacts/urls.py
+++ contacts/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
 	url(r'', views.ListContactView.as_view(), name='contacts-list'),
 	url(r'^new/$', views.CreateContactView.as_view(), name='contacts-new'),	
 	url(r'^edit/$', views.EditContactView.as_view(), name='contacts-edit'),
+	url(r'^(?P<pk>\d+)/$', contacts.views.ContactView.as_view(), name='contacts-view',),
+	url(r'^delete/(?P<pk>\d+)/$', contacts.views.DeleteContactView.as_view(), name='contacts-delete',),
+	url(r'^edit/(?P<pk>\d+)/$', contacts.views.UpdateContactView.as_view(), name='contacts-edit',),
+
+
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -7,11 +7,19 @@ class ListContactView(ListView):
 	template_name='contact_list.html'
 
 class CreateContactView(CreateView):
-	model=Contact
-	template_name='edit_contact.html'
 
-	def get_success_url(self):
-		return reverse('contacts-list')
+    model = Contact
+    template_name = 'edit_contact.html'
+
+    def get_success_url(self):
+        return reverse('contacts-list')
+
+    def get_context_data(self, **kwargs):
+
+        context = super(CreateContactView, self).get_context_data(**kwargs)
+        context['action'] = reverse('contacts-new')
+
+        return context
 
 class EditContactView(UpdateView):
 	model=Contact
@@ -19,4 +27,32 @@ class EditContactView(UpdateView):
 
 	def get_success_url(self):
 		return reverse('contacts-list')
+
+class UpdateContactView(UpdateView):
+
+    model = Contact
+    template_name = 'edit_contact.html'
+
+    def get_success_url(self):
+        return reverse('contacts-list')
+
+    def get_context_data(self, **kwargs):
+
+        context = super(UpdateContactView, self).get_context_data(**kwargs)
+        context['action'] = reverse('contacts-edit',
+                                    kwargs={'pk': self.get_object().id})
+
+        return context
+class DeleteContactView(DeleteView):
+
+    model = Contact
+    template_name = 'delete_contact.html'
+
+    def get_success_url(self):
+        return reverse('contacts-list')
+
+class ContactView(DetailView):
+
+    model = Contact
+    template_name = 'contact.html'
 
