36c4676c9601cff4317756b44ea6c64aeaa65744
ankush <ankush.1996@gmail.com>
2015-07-07 00:25:01 +0530

    views made for new loginapp

--- login_new/views.py
+++ login_new/views.py
@@ -2,18 +2,19 @@ from django.shortcuts import render
 from django.views.generic import DetailView, CreateView, UpdateView, View
 from .models import Profile
 import forms
+from django.core.urlresolvers import reverse
 
 class CreateProfileView(CreateView):
 	model=Profile
 	template_name='signup.html' 
-	form_class=form.ProfileForm
+	form_class=forms.ProfileForm
 	def get_success_url(self):
 		return reverse('home')
 
 class UpdateProfileView(UpdateView):
 	model=Profile
 	template_name='update.html'
-	form_class=form.ProfileForm
+	form_class=forms.ProfileForm
 	def get_success_url(self):
 		return reverse('home')
 
@@ -21,6 +22,33 @@ class ProfileView(DetailView):
 	model=Profiel
 	template_name='profile.html'
 
+class LoginView(CreateView):
+	model=Profile
+	template_name='login.html'
+	form_class=forms.LoginForm
+	def get_success_url(self,request):
+		def post(request):
+			form=forms.LoginForm(request.POST)
+			if form.is_valid():
+				data=form.cleaned_data
+				user=get_object_or_404(Profile,data['username'])
+				if user.password == data['password']:
+					request.session['user']=user.username
+					return reverse('home')
+				else:
+					return reverse('login')
+			else:
+				return reverse('login')		
+
+class LogoutView(View):
+	model=Profile
+	template_name='logout.html'
+	def get_success_url(self,request):
+		del request.session['user']
+		return reverse('home')
+
 class HomeView(View):
-	def post(self,request):
-		
+	model=Profile
+	template_name='home.html'
+	def get_success_url(self):
+		return reverse('home')
